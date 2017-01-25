46b8c5b199e4b5fd9931a41e484a05508fa82b59
Peter Laraia <pvlaraia@gmail.com>
2017-01-25 09:07:12 -0600

    PL - changing rollup config & additonal env setup. Lot of new stuff to learn about

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,8 @@
 export default {
-    entry: 'dist/index.js',
-    dest: 'dist/bundles/angular2-datetime.umd.js',
-    sourceMap: false,
     format: 'umd',
     moduleName: 'angular2-datetime',
     external: [
-        '@angular/core'    
+        '@angular/core',
+        '@angular/common'
     ]
 }
