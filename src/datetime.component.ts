846a8162ceb2cef3457f085d183362a446ae467a
Peter Laraia <pvlaraia@gmail.com>
2017-01-24 19:03:56 -0800

    PL - root skeleton

---
+++ src/datetime.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: '[datetime-picker]',
+    templateUrl: 'datetime.component.html'
+})
+export class DatetimeComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
---
+++ src/datetime.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { DatetimeComponent }   from './datetime.component';
+
+@NgModule({
+    imports: [],
+    exports: [],
+    declarations: [DatetimeComponent],
+    providers: [],
+})
+export class Angular2DatetimeModule { }
