9386f30a77a8a5328bd05e1d4c36b5a757cdf4a0
Peter Laraia <pvlaraia@gmail.com>
2017-01-27 22:37:54 -0800

    PL - ok cool styles working now too. needed to move them to the right place...

--- src/datetime.component.ts
+++ src/datetime.component.ts
@@ -12,28 +12,7 @@ import { Component, ElementRef, Input, HostListener, OnInit, ViewChild } from '@
         <time-picker *ngIf="showTime"></time-picker>
         <input (focus)="showTime = true" type="text" />
     </div>
-    `,
-    styles: [`
-        .date-picker {
-            position: absolute;
-            display: block;
-            min-width:100px;
-            text-align: center;
-            background: white;
-            border: 1px solid rgba(0, 0, 0, .15);
-            box-shadow: 0 6px 12px rgba(0, 0, 0, .175);
-            top: 20px;
-            z-index:10;
-        },
-        :host /deep/ .calendar-btn {
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            -khtml-user-select: none;
-            -ms-user-select: none;
-            
-            cursor: pointer;
-        }
-    `]
+    `
 })
 export class DatetimeComponent {
     @Input() options: any = {
