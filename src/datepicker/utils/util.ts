6f535edf10cbd4b2e60fec17c7a03bda1c6265df
Peter Laraia <pvlaraia@gmail.com>
2017-01-31 21:54:58 -0600

    PL - modified calendar building logic s.t. a month can have varying number of rows based on what is needed

--- src/datepicker/utils/util.ts
+++ src/datepicker/utils/util.ts
@@ -2,24 +2,22 @@ export class DateUtils {
  static buildCalendar(month: number, year: number): Date[][] {
     let calendar: any[] = []; //2d array of ints
     let date: Date = new Date(year, month, 1);
-    //walk it back
-    date.setDate(date.getDate() - 1);
+    //walk it back if Sunday is not the first of the month
     while(date.getDay() > 0) {
       date.setDate(date.getDate() - 1);
     }
+    
     /*
     * Fill in the calendar
-    * Will always be 6 weeks, we want to show at least 1 day from
-    * prev & next month
     */
-    for(let wk = 0; wk < 6; wk++) {
+    do {
       let week: Date[] = [];
       for(let dow = 0; dow < 7; dow++) {
         week.push(new Date(date.getFullYear(), date.getMonth(), date.getDate()));
         date.setDate(date.getDate() + 1);
       }
       calendar.push(week);
-    }
+    } while (date.getMonth() === month);
     return calendar;
   }
 }
