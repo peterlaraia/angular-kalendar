b61f0a8ad01bf01c6f27e951e76e23b9fb7451df
Peter Laraia <PeterLaraiaGit@gmail.com>
2017-02-19 02:38:41 -0600

    Fixing rollup & dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,11 @@
 export default {
+    entry: './dist/index.js',
+    dest: './dist/bundles/angular-kalendar.umd.js',
     format: 'umd',
     moduleName: 'angular-kalendar',
     external: [
         '@angular/core',
-        '@angular/common'
+        '@angular/common',
+        '@angular/forms'
     ]
 }
