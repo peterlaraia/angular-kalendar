5d6c551a4a99b7882e2abe935a58ac9f0d8bb803
Peter Laraia <PeterLaraiaGit@gmail.com>
2017-04-23 18:47:57 -0500

    increasing coverage on date formatter & years component

--- test/datepicker/datepicker-years.component.spec.ts
+++ test/datepicker/datepicker-years.component.spec.ts
@@ -24,125 +24,165 @@ describe('DatePicker Years Component', () => {
         de = fixture.debugElement;
     });
 
-    it('should create years view on init', () => {
-        comp.displayDate = new Date('2012-10-13T12:00');
-        fixture.detectChanges();
-        expect(comp.years).toEqual([
-            2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019
-        ]);
+    describe('Decade View', () => {
+        it('should create years view on init', () => {
+            comp.displayDate = new Date('2012-10-13T12:00');
+            fixture.detectChanges();
+            expect(comp.years).toEqual([
+                2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019
+            ]);
+        });
+
+        it('should select a year', () => {
+            comp.displayDate = new Date('2012-10-13T12:00');
+            let spy = spyOn(comp, 'updateYear').and.callThrough();
+            fixture.detectChanges();
+            expect(comp.displayDate.getFullYear()).toBe(2012);
+            let selected: DebugElement = de.query(By.css('.ang2cal-selected'));
+            expect(selected.nativeElement.textContent.trim()).toEqual('2012');
+
+            let cells: DebugElement[] = de.queryAll(By.css('td'));
+            expect(cells.length).toBe(10);
+            cells[6].nativeElement.click();
+            fixture.detectChanges();
+            selected = de.query(By.css('.ang2cal-selected'));
+            expect(spy.calls.any()).toBeTruthy();
+            expect(selected.nativeElement.textContent.trim()).toEqual('2016');
+            expect(comp.displayDate.getFullYear()).toBe(2016);
+        });
+
+        it('should display the previous decade', () => {
+            comp.displayDate = new Date('2012-10-13T12:00');
+            let spy = spyOn(comp, 'prev').and.callThrough();
+            fixture.detectChanges();
+            let displaying: DebugElement = de.query(By.css('thead th div'));
+            expect(displaying.nativeElement.textContent.trim()).toContain('2010', '2020');
+            let btn: DebugElement = de.query(By.css('.ang2cal-prev-btn'));
+            btn.nativeElement.click();
+            fixture.detectChanges();
+
+            displaying = de.query(By.css('thead th div'));
+            expect(displaying.nativeElement.textContent.trim()).toContain('2000', '2010');
+            expect(spy.calls.any()).toBeTruthy();
+            expect(comp.years).toEqual([
+                2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009
+            ]);
+        });
+
+        it('should display the next decade', () => {
+            comp.displayDate = new Date('2012-10-13T12:00');
+            let spy = spyOn(comp, 'next').and.callThrough();
+            fixture.detectChanges();
+            let displaying: DebugElement = de.query(By.css('thead th div'));
+            expect(displaying.nativeElement.textContent.trim()).toContain('2010', '2020');
+            let btn: DebugElement = de.query(By.css('.ang2cal-next-btn'));
+            btn.nativeElement.click();
+            fixture.detectChanges();
+
+            displaying = de.query(By.css('thead th div'));
+            expect(displaying.nativeElement.textContent.trim()).toContain('2020', '2030');
+            expect(spy.calls.any()).toBeTruthy();
+            expect(comp.years).toEqual([
+                2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029
+            ]);
+        });
+
+        it('should emit view change event', () => {
+            comp.viewChange.subscribe((view: View) => expect(view).toBe(View.Decades));
+            comp.displayDate = new Date('2012-10-13T12:00');
+            let spy = spyOn(comp, 'onViewHigher').and.callThrough();
+            fixture.detectChanges();
+            let btn: DebugElement = de.query(By.css('th.ang2cal-selectable'));
+            btn.nativeElement.click();
+            fixture.detectChanges();
+            expect(spy.calls.any()).toBeTruthy();
+        });
     });
 
-    it('should update the display date', () => {
-        comp.displayDate = new Date('2012-10-13T12:00');
-        let spy = spyOn(comp, 'updateYear').and.callThrough();
-        fixture.detectChanges();
-        expect(comp.displayDate.getFullYear()).toBe(2012);
-        let selected: DebugElement = de.query(By.css('.ang2cal-selected'));
-        expect(selected.nativeElement.textContent.trim()).toEqual('2012');
+    describe('Century View', () => {
+        it('should select a decade', () => {
+            comp.centuryView = true;
+            comp.displayDate = new Date('2012-10-13T12:00');
+            let spy = spyOn(comp, 'updateYear').and.callThrough();
+            fixture.detectChanges();
+            expect(comp.displayDate.getFullYear()).toBe(2012);
+            let selected: DebugElement = de.query(By.css('.ang2cal-selected'));
+            expect(selected.nativeElement.textContent.trim()).toEqual('2010');
 
-        let cells: DebugElement[] = de.queryAll(By.css('td'));
-        expect(cells.length).toBe(10);
-        cells[6].nativeElement.click();
-        fixture.detectChanges();
-        selected = de.query(By.css('.ang2cal-selected'));
-        expect(spy.calls.any()).toBeTruthy();
-        expect(selected.nativeElement.textContent.trim()).toEqual('2016');
-        expect(comp.displayDate.getFullYear()).toBe(2016);
+            let cells: DebugElement[] = de.queryAll(By.css('td'));
+            expect(cells.length).toBe(10);
+            cells[2].nativeElement.click(); // 2020
+            fixture.detectChanges();
+            selected = de.query(By.css('.ang2cal-selected'));
+            expect(spy.calls.any()).toBeTruthy();
+            expect(selected.nativeElement.textContent.trim()).toEqual('2020');
+            expect(comp.displayDate.getFullYear()).toBe(2020);
+        });
+
+        it('should select the same decade', () => {
+            comp.centuryView = true;
+            comp.displayDate = new Date('2012-10-13T12:00');
+            let spy = spyOn(comp, 'updateYear').and.callThrough();
+            fixture.detectChanges();
+            expect(comp.displayDate.getFullYear()).toBe(2012);
+            let selected: DebugElement = de.query(By.css('.ang2cal-selected'));
+            expect(selected.nativeElement.textContent.trim()).toEqual('2010');
+
+            let cells: DebugElement[] = de.queryAll(By.css('td'));
+            expect(cells.length).toBe(10);
+            cells[1].nativeElement.click(); // 2010
+            fixture.detectChanges();
+            selected = de.query(By.css('.ang2cal-selected'));
+            expect(spy.calls.any()).toBeTruthy();
+            expect(selected.nativeElement.textContent.trim()).toEqual('2010');
+            expect(comp.displayDate.getFullYear()).toBe(2012);
+        });
+
+        it('should create decade view on init', () => {
+            comp.centuryView = true;
+            comp.displayDate = new Date('2012-10-13T12:00');
+            fixture.detectChanges();
+            expect(comp.years).toEqual([
+                2000, 2010, 2020, 2030, 2040, 2050, 2060, 2070, 2080, 2090
+            ]);
+        });
+
+        it('should display the previous century', () => {
+            comp.centuryView = true;
+            comp.displayDate = new Date('2012-10-13T12:00');
+            let spy = spyOn(comp, 'prev').and.callThrough();
+            fixture.detectChanges();
+            let displaying: DebugElement = de.query(By.css('thead th div'));
+            expect(displaying.nativeElement.textContent.trim()).toContain('2000', '2100');
+            let btn: DebugElement = de.query(By.css('.ang2cal-prev-btn'));
+            btn.nativeElement.click();
+            fixture.detectChanges();
+
+            displaying = de.query(By.css('thead th div'));
+            expect(displaying.nativeElement.textContent.trim()).toContain('1900', '2000');
+            expect(spy.calls.any()).toBeTruthy();
+            expect(comp.years).toEqual([
+                1900, 1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990
+            ]);
+        });
+
+        it('should display the next century', () => {
+            comp.centuryView = true;
+            comp.displayDate = new Date('2012-10-13T12:00');
+            let spy = spyOn(comp, 'next').and.callThrough();
+            fixture.detectChanges();
+            let displaying: DebugElement = de.query(By.css('thead th div'));
+            expect(displaying.nativeElement.textContent.trim()).toContain('2000', '2100');
+            let btn: DebugElement = de.query(By.css('.ang2cal-next-btn'));
+            btn.nativeElement.click();
+            fixture.detectChanges();
+
+            displaying = de.query(By.css('thead th div'));
+            expect(displaying.nativeElement.textContent.trim()).toContain('2100', '2200');
+            expect(spy.calls.any()).toBeTruthy();
+            expect(comp.years).toEqual([
+                2100, 2110, 2120, 2130, 2140, 2150, 2160, 2170, 2180, 2190
+            ]);
+        });
     });
-
-    it('should display the previous decade', () => {
-        comp.displayDate = new Date('2012-10-13T12:00');
-        let spy = spyOn(comp, 'prev').and.callThrough();
-        fixture.detectChanges();
-        let displaying: DebugElement = de.query(By.css('thead th div'));
-        expect(displaying.nativeElement.textContent.trim()).toContain('2010', '2020');
-        let btn: DebugElement = de.query(By.css('.ang2cal-prev-btn'));
-        btn.nativeElement.click();
-        fixture.detectChanges();
-
-        displaying = de.query(By.css('thead th div'));
-        expect(displaying.nativeElement.textContent.trim()).toContain('2000', '2010');
-        expect(spy.calls.any()).toBeTruthy();
-        expect(comp.years).toEqual([
-            2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009
-        ]);
-    });
-
-    it('should display the next decade', () => {
-        comp.displayDate = new Date('2012-10-13T12:00');
-        let spy = spyOn(comp, 'next').and.callThrough();
-        fixture.detectChanges();
-        let displaying: DebugElement = de.query(By.css('thead th div'));
-        expect(displaying.nativeElement.textContent.trim()).toContain('2010', '2020');
-        let btn: DebugElement = de.query(By.css('.ang2cal-next-btn'));
-        btn.nativeElement.click();
-        fixture.detectChanges();
-
-        displaying = de.query(By.css('thead th div'));
-        expect(displaying.nativeElement.textContent.trim()).toContain('2020', '2030');
-        expect(spy.calls.any()).toBeTruthy();
-        expect(comp.years).toEqual([
-            2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029
-        ]);
-    });
-
-    it('should emit view change event', () => {
-        comp.viewChange.subscribe((view: View) => expect(view).toBe(View.Decades));
-        comp.displayDate = new Date('2012-10-13T12:00');
-        let spy = spyOn(comp, 'onViewHigher').and.callThrough();
-        fixture.detectChanges();
-        let btn: DebugElement = de.query(By.css('th.ang2cal-selectable'));
-        btn.nativeElement.click();
-        fixture.detectChanges();
-        expect(spy.calls.any()).toBeTruthy();
-    });
-
-
-    it('should create decade view on init', () => {
-        comp.centuryView = true;
-        comp.displayDate = new Date('2012-10-13T12:00');
-        fixture.detectChanges();
-        expect(comp.years).toEqual([
-            2000, 2010, 2020, 2030, 2040, 2050, 2060, 2070, 2080, 2090
-        ]);
-    });
-
-    it('should display the previous century', () => {
-        comp.centuryView = true;
-        comp.displayDate = new Date('2012-10-13T12:00');
-        let spy = spyOn(comp, 'prev').and.callThrough();
-        fixture.detectChanges();
-        let displaying: DebugElement = de.query(By.css('thead th div'));
-        expect(displaying.nativeElement.textContent.trim()).toContain('2000', '2100');
-        let btn: DebugElement = de.query(By.css('.ang2cal-prev-btn'));
-        btn.nativeElement.click();
-        fixture.detectChanges();
-
-        displaying = de.query(By.css('thead th div'));
-        expect(displaying.nativeElement.textContent.trim()).toContain('1900', '2000');
-        expect(spy.calls.any()).toBeTruthy();
-        expect(comp.years).toEqual([
-            1900, 1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990
-        ]);
-    });
-
-    it('should display the next century', () => {
-        comp.centuryView = true;
-        comp.displayDate = new Date('2012-10-13T12:00');
-        let spy = spyOn(comp, 'next').and.callThrough();
-        fixture.detectChanges();
-        let displaying: DebugElement = de.query(By.css('thead th div'));
-        expect(displaying.nativeElement.textContent.trim()).toContain('2000', '2100');
-        let btn: DebugElement = de.query(By.css('.ang2cal-next-btn'));
-        btn.nativeElement.click();
-        fixture.detectChanges();
-
-        displaying = de.query(By.css('thead th div'));
-        expect(displaying.nativeElement.textContent.trim()).toContain('2100', '2200');
-        expect(spy.calls.any()).toBeTruthy();
-        expect(comp.years).toEqual([
-            2100, 2110, 2120, 2130, 2140, 2150, 2160, 2170, 2180, 2190
-        ]);
-    });
-
 });
--- test/datepicker/utils/date-formatter.spec.ts
+++ test/datepicker/utils/date-formatter.spec.ts
@@ -1,70 +1,93 @@
 import { DateFormatter } from '../../../lib/datepicker/utils/date-formatter';
 
 describe('Date Formatter', () => {
-    it('should format m/d/yyyy', () => {
-        let date = new Date('1994-03-21T12:00');
-        let formattedStr = DateFormatter.formatDate(date, 'm/d/yyyy');
-        expect(formattedStr).toEqual('3/21/1994');
+    describe('Dates', () => {
+        it('should format m/d/yyyy', () => {
+            let date = new Date('1994-03-21T12:00');
+            let formattedStr = DateFormatter.formatDate(date, 'm/d/yyyy');
+            expect(formattedStr).toEqual('3/21/1994');
+        });
+
+        it('should format mm/dd/yyyy', () => {
+            let date = new Date('1994-03-21T12:00');
+            let formattedStr = DateFormatter.formatDate(date, 'mm/dd/yyyy');
+            expect(formattedStr).toEqual('03/21/1994');
+        });
+
+        it('should format m/dd/yyyy', () => {
+            let date = new Date('1974-11-02T12:00');
+            let formattedStr = DateFormatter.formatDate(date, 'm/dd/yyyy');
+            expect(formattedStr).toEqual('11/02/1974');
+        });
+
+        it('should format m/dd/yy pre 2000s', () => {
+            let date = new Date('1974-11-02T12:00');
+            let formattedStr = DateFormatter.formatDate(date, 'm/dd/yy');
+            expect(formattedStr).toEqual('11/02/74');
+        });
+
+        it('should format m/dd/yy post 2000s', () => {
+            let date = new Date('2025-05-22T12:00');
+            let formattedStr = DateFormatter.formatDate(date, 'm/dd/yy');
+            expect(formattedStr).toEqual('5/22/25');
+        });
+
+        it('should format shortMonth d, yyyy', () => {
+            let date = new Date('2015-08-03T12:00');
+            let formattedStr = DateFormatter.formatDate(date, 'mmm d, yyyy');
+            expect(formattedStr).toEqual('Aug 3, 2015');
+        });
+
+        it('should format shortMonth dd, yyyy', () => {
+            let date = new Date('2015-06-09T12:00');
+            let formattedStr = DateFormatter.formatDate(date, 'mmm dd, yyyy');
+            expect(formattedStr).toEqual('Jun 09, 2015');
+        });
+
+        it('should format longMonth dd, yyyy', () => {
+            let date = new Date('2015-07-08T12:00');
+            let formattedStr = DateFormatter.formatDate(date, 'mmmm dd, yyyy');
+            expect(formattedStr).toEqual('July 08, 2015');
+        });
+
+        it('should format longMonth d, yy', () => {
+            let date = new Date('2013-03-25T12:00');
+            let formattedStr = DateFormatter.formatDate(date, 'mmmm d, yy');
+            expect(formattedStr).toEqual('March 25, 13');
+        });
     });
 
-    it('should format mm/dd/yyyy', () => {
-        let date = new Date('1994-03-21T12:00');
-        let formattedStr = DateFormatter.formatDate(date, 'mm/dd/yyyy');
-        expect(formattedStr).toEqual('03/21/1994');
+    describe('Date & Times', () => {
+        it('should format m/d/yyyy HH:MM in 24hr time', () => {
+            let date = new Date('21 Mar 1994 13:00');
+            let formattedStr = DateFormatter.formatDate(date, 'm/d/yyyy HH:MM');
+            expect(formattedStr).toEqual('3/21/1994 13:00');
+        });
+
+        it('should format m/d/yyyy H:MM in 12hr time', () => {
+            let date = new Date('21 Mar 1994 13:00');
+            let formattedStr = DateFormatter.formatDate(date, 'm/d/yyyy H:MM', false);
+            expect(formattedStr).toEqual('3/21/1994 1:00 PM');
+        });
+
+        it('should format m/d/yyyy HH:MM in 12hr time', () => {
+            let date = new Date('21 Mar 1994 13:00');
+            let formattedStr = DateFormatter.formatDate(date, 'm/d/yyyy HH:MM', false);
+            expect(formattedStr).toEqual('3/21/1994 01:00 PM');
+        });
     });
 
-    it('should format m/dd/yyyy', () => {
-        let date = new Date('1974-11-02T12:00');
-        let formattedStr = DateFormatter.formatDate(date, 'm/dd/yyyy');
-        expect(formattedStr).toEqual('11/02/1974');
-    });
+    describe('Bad Input', () => {
+        it('should allow the user to format weird input, not gonna double check their work', () => {
+            let date = new Date('2015-07-08T12:00');
+            let formattedStr = DateFormatter.formatDate(date, 'yymmmddmmyyyy');
+            expect(formattedStr).toEqual('15Jul08072015');
+        });
 
-    it('should format m/dd/yy pre 2000s', () => {
-        let date = new Date('1974-11-02T12:00');
-        let formattedStr = DateFormatter.formatDate(date, 'm/dd/yy');
-        expect(formattedStr).toEqual('11/02/74');
+        it('should allow the user to format weird input, not gonna double check their work 2', () => {
+            let date = new Date('2015-07-08T12:00');
+            let formattedStr = DateFormatter.formatDate(date, 'yyxmmxmd34dmzzmyyyyWOW');
+            expect(formattedStr).toEqual('15x07x783487zz72015WOW');
+        });
     });
-
-    it('should format m/dd/yy post 2000s', () => {
-        let date = new Date('2025-05-22T12:00');
-        let formattedStr = DateFormatter.formatDate(date, 'm/dd/yy');
-        expect(formattedStr).toEqual('5/22/25');
-    });
-
-    it('should format shortMonth d, yyyy', () => {
-        let date = new Date('2015-08-03T12:00');
-        let formattedStr = DateFormatter.formatDate(date, 'mmm d, yyyy');
-        expect(formattedStr).toEqual('Aug 3, 2015');
-    });
-
-    it('should format shortMonth dd, yyyy', () => {
-        let date = new Date('2015-06-09T12:00');
-        let formattedStr = DateFormatter.formatDate(date, 'mmm dd, yyyy');
-        expect(formattedStr).toEqual('Jun 09, 2015');
-    });
-
-    it('should format longMonth dd, yyyy', () => {
-        let date = new Date('2015-07-08T12:00');
-        let formattedStr = DateFormatter.formatDate(date, 'mmmm dd, yyyy');
-        expect(formattedStr).toEqual('July 08, 2015');
-    });
-
-    it('should format longMonth d, yy', () => {
-        let date = new Date('2013-03-25T12:00');
-        let formattedStr = DateFormatter.formatDate(date, 'mmmm d, yy');
-        expect(formattedStr).toEqual('March 25, 13');
-    });
-
-    it('should allow the user to format weird input, not gonna double check their work', () => {
-        let date = new Date('2015-07-08T12:00');
-        let formattedStr = DateFormatter.formatDate(date, 'yymmmddmmyyyy');
-        expect(formattedStr).toEqual('15Jul08072015');
-    });
-
-    it('should allow the user to format weird input, not gonna double check their work 2', () => {
-        let date = new Date('2015-07-08T12:00');
-        let formattedStr = DateFormatter.formatDate(date, 'yyxmmxmd34dmzzmyyyyWOW');
-        expect(formattedStr).toEqual('15x07x783487zz72015WOW');
-    });
-
 });
